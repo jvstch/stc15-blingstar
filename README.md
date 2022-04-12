8e0adb9f9f8583615628d2032f72b6eefc688609
jeevasdev <jeevasdev@protomail.com>
2022-04-12 10:20:59 +1000

    Kind of working.

--- README.md
+++ README.md
@@ -1,14 +1,14 @@
-# stc89-blingheart
-Open firmware for STC89C52RC based Commonly available Heart Shaped LED PCB kit 
+# stc15-blingstar
+Open firmware for STC15F204EA based Commonly available Star Shaped LED PCB kit 
 
 ## Description
-  STC89C52RC in one of the best and faithful implementation of the classic intel 80C51 micro
-  controller. This chip comes with the very basic peripherals like the original. The key 
-  features are 4 ttl-level 8-bit ports, ADC, UART, WDT and the killer feature is the easy 
-  programmability using very minimal ttl-level serial port.
+  STC15F204EA in one of the fast implementation(1 instruction per clock) of the classic intel 80C51 micro
+  controller. This chip comes with many classic peripherals like ADC's UART, Timers, Interrupts etc but don't
+  include any modern purpose built ports like I2C,SPI,PWM etc. The key 
+  features are 4 ttl-level ports(P0-2bits, P1-8bits, P2-8bits and P3-8bits), and the killer feature is the easy programmability using very minimal ttl-level serial port.
   
  ## Wiring for Flashing the Chip
-  If you have a board with any STC 8-bit controller like this 89C52 or any of the 12x or 15x
+  If you have a board with any STC 8-bit controller like this STC15F204EA or any of the 89x, stc8, 12x or 15x
   series the only thing you need to program is a $2 USB-Serial converter and connect the 4
   wires as below:
   
@@ -24,4 +24,7 @@ Open firmware for STC89C52RC based Commonly available Heart Shaped LED PCB kit
 ## Development tools (Fully Open Source)  - Prefer Linux.
   This code can be easily developed using the fully opensource VSCodium IDE
   with PlatformIO extension which inturn uses the fully open source excellent sdcc toolchain along
-  with the open source python based stcgal programmer.
+  with the open source python based stcgal programmer or the C based stc8prog.
+### Programmer links:
+  https://github.com/grigorig/stcgal
+  https://github.com/IOsetting/stc8prog/releases
--- src/bling-star.c
+++ src/bling-star.c
@@ -18,7 +18,9 @@
 */
 
 /*********************************************************************************************\
- * STC15F204EA based Star shaped LED PCB widely available in kit form
+ * STC15F204EA based Star shaped LED PCB widely available in kit form.
+ * There are a total of 50 led's in sets of 2 which in turn is controlled by 25 port bits of the 
+ * MCU.
  *
  * STC15F204EA in one of the fast implementation(1 instruction per clock) of the classic intel 80C51 micro
  * controller. This chip comes with many classic peripherals like ADC's UART, Timers, Interrupts etc but don't
@@ -44,17 +46,17 @@
 \*********************************************************************************************/
 
 #define FOSC 11952000UL //11.952 Mhz
-#define FDIV 70
+#define FDIV 700
 
 
 #include <stdint.h>
 #include <stdio.h>
 #include <stc15fxx.h>
 
-#define HEART_UL P0
-#define HEART_LL P1
-#define HEART_UR P2
-#define HEART_LR P3
+#define STAR_UL P0
+#define STAR_LL P1
+#define STAR_UR P2
+#define STAR_LR P3
 
 #define SEG_OFF 0xFF
 #define SEG_ON 0x0
@@ -73,64 +75,64 @@ void delay_ms(unsigned int ms)
 }
 
 /*********************************************************************************************/
-void bh_all_off()
+void bs_all_off()
 {
-        HEART_LR = SEG_OFF;
-        HEART_LL = SEG_OFF;
-        HEART_UR = SEG_OFF;
-        HEART_UL = SEG_OFF;
+        STAR_LR = SEG_OFF;
+        STAR_LL = SEG_OFF;
+        STAR_UR = SEG_OFF;
+        STAR_UL = SEG_OFF;
      
 }
 
 /*********************************************************************************************/
-void bh_all_on()
+void bs_all_on()
 {
-        HEART_LR = SEG_ON;
-        HEART_LL = SEG_ON;
-        HEART_UR = SEG_ON;
-        HEART_UL = SEG_ON;
+        STAR_LR = SEG_ON;
+        STAR_LL = SEG_ON;
+        STAR_UR = SEG_ON;
+        STAR_UL = SEG_ON;
      
 }
 
 /*********************************************************************************************/
-void bh_all_toggle()
+void bs_all_toggle()
 {
-        HEART_LR = ~HEART_LR;
-        HEART_LL = ~HEART_LL;
-        HEART_UR = ~HEART_UR;
-        HEART_UL = ~HEART_UL;
+        STAR_LR = ~STAR_LR;
+        STAR_LL = ~STAR_LL;
+        STAR_UR = ~STAR_UR;
+        STAR_UL = ~STAR_UL;
      
 }
 
 /*********************************************************************************************/
-void bh_setup()
+void bs_setup()
 {
-        bh_all_off();
+        bs_all_off();
         delay_ms(50);
    
 }
 
 /*********************************************************************************************/
-void bh_quadrant_dance(unsigned int count)
+void bs_quadrant_dance(unsigned int count)
 {
         for (unsigned int i=0; i<count; i++){  
 
-        bh_all_off();
+        bs_all_off();
         delay_ms(250);
 
-        HEART_UL = SEG_ON;
-        HEART_LR = SEG_ON;        
+        STAR_UL = SEG_ON;
+        STAR_LR = SEG_ON;        
         delay_ms(250);
-        bh_all_toggle();
+        bs_all_toggle();
         delay_ms(250);
         
-        bh_all_off();  
+        bs_all_off();  
         delay_ms(250);
 
-        HEART_LL = SEG_ON;
-        HEART_UR = SEG_ON;        
+        STAR_LL = SEG_ON;
+        STAR_UR = SEG_ON;        
         delay_ms(250);
-        bh_all_toggle(); 
+        bs_all_toggle(); 
         delay_ms(250);
  
      } 
@@ -138,164 +140,164 @@ void bh_quadrant_dance(unsigned int count)
 }
 
 /*********************************************************************************************/
-void bh_quadrant_chase(unsigned int count)
+void bs_quadrant_chase(unsigned int count)
 {
-    bh_all_off();
+    bs_all_off();
     for (unsigned int i=0; i<count; i++){  
-        HEART_UL = SEG_ON;
+        STAR_UL = SEG_ON;
          delay_ms(250);
-        HEART_LL = SEG_ON;
+        STAR_LL = SEG_ON;
          delay_ms(250);
-        HEART_UR = SEG_ON;
+        STAR_UR = SEG_ON;
          delay_ms(250);
-        HEART_LR = SEG_ON;        
+        STAR_LR = SEG_ON;        
         delay_ms(250);
 
-        HEART_UL = SEG_OFF;
+        STAR_UL = SEG_OFF;
          delay_ms(250);
-        HEART_LL = SEG_OFF;
+        STAR_LL = SEG_OFF;
          delay_ms(250);
-        HEART_UR = SEG_OFF;
+        STAR_UR = SEG_OFF;
          delay_ms(250);
-        HEART_LR = SEG_OFF;        
+        STAR_LR = SEG_OFF;        
         delay_ms(250);
      }    
 }   
 
 /*********************************************************************************************/
-void bh_fullon_chase_cw(unsigned int count)
+void bs_fullon_chase_cw(unsigned int count)
 {
-    bh_all_off(); 
+    bs_all_off(); 
     for (unsigned int i=0; i<count; i++){ 
 
         for (unsigned int j=0; j<8; j++){  
-            HEART_UR = ~(0x1U << (7-j));
+            STAR_UR = ~(0x1U << (7-j));
             delay_ms(50);      
 
         }
-        HEART_UR = SEG_OFF;
+        STAR_UR = SEG_OFF;
 
         for (unsigned int j=0; j<8; j++){  
-            HEART_LR = ~(0x1U << (7-j)); 
+            STAR_LR = ~(0x1U << (7-j)); 
             delay_ms(50);     
         }
-        HEART_LR = SEG_OFF;
+        STAR_LR = SEG_OFF;
 
         for (unsigned int j=0; j<8; j++){  
-            HEART_LL = ~(0x1U << (7-j));  
+            STAR_LL = ~(0x1U << (7-j));  
             delay_ms(50);    
         }
-        HEART_LL = SEG_OFF;
+        STAR_LL = SEG_OFF;
 
         for (unsigned int j=0; j<8; j++){  
-            HEART_UL = ~(0x1U << j); 
+            STAR_UL = ~(0x1U << j); 
             delay_ms(50);     
 
         }
-        HEART_UL = SEG_OFF;
+        STAR_UL = SEG_OFF;
 
     }   
 }
 
 /*********************************************************************************************/
-void bh_fullon_chase_ccw(unsigned int count)
+void bs_fullon_chase_ccw(unsigned int count)
 {
-    bh_all_off();
+    bs_all_off();
     for (unsigned int i=0; i<count; i++){ 
 
         for (unsigned int j=0; j<8; j++){  
-            HEART_UL = ~(0x1U << (7-j)); 
+            STAR_UL = ~(0x1U << (7-j)); 
             delay_ms(50);     
 
         }
-        HEART_UL = SEG_OFF;
+        STAR_UL = SEG_OFF;
             
         for (unsigned int j=0; j<8; j++){  
-           HEART_LL = ~(0x1U << (j));  
+           STAR_LL = ~(0x1U << (j));  
            delay_ms(50);    
         }
-        HEART_LL = SEG_OFF;    
+        STAR_LL = SEG_OFF;    
         
         for (unsigned int j=0; j<8; j++){  
-           HEART_LR = ~(0x1U << (j)); 
+           STAR_LR = ~(0x1U << (j)); 
             delay_ms(50);     
         }
-        HEART_LR = SEG_OFF;
+        STAR_LR = SEG_OFF;
 
         for (unsigned int j=0; j<8; j++){  
-            HEART_UR = ~(0x1U << (j));
+            STAR_UR = ~(0x1U << (j));
             delay_ms(50);      
 
         }
-        HEART_UR = SEG_OFF;
+        STAR_UR = SEG_OFF;
 
     } 
 
 }
 
 /*********************************************************************************************/
-void bh_fullon_trace_cw(unsigned int count)
+void bs_fullon_trace_cw(unsigned int count)
 {
-    bh_all_off();
+    bs_all_off();
     for (unsigned int i=0; i<count; i++){ 
 
         for (unsigned int j=0; j<8; j++){  
-            HEART_UR &= ~(0x1U << (7-j));
+            STAR_UR &= ~(0x1U << (7-j));
             delay_ms(50);      
 
         }
 
         for (unsigned int j=0; j<8; j++){  
-            HEART_LR &= ~(0x1U << (7-j)); 
+            STAR_LR &= ~(0x1U << (7-j)); 
             delay_ms(50);     
 
         }
 
         for (unsigned int j=0; j<8; j++){  
-            HEART_LL &= ~(0x1U << (7-j));  
+            STAR_LL &= ~(0x1U << (7-j));  
             delay_ms(50);    
 
         }
 
         for (unsigned int j=0; j<8; j++){  
-            HEART_UL &= ~(0x1U << j); 
+            STAR_UL &= ~(0x1U << j); 
             delay_ms(50);     
 
         }
-        bh_all_off();
+        bs_all_off();
         delay_ms(50);
 
     }   
 }
 
 /*********************************************************************************************/
-void bh_fullon_trace_ccw(unsigned int count)
+void bs_fullon_trace_ccw(unsigned int count)
 {
-    bh_all_off();
+    bs_all_off();
     for (unsigned int i=0; i<count; i++){ 
 
         for (unsigned int j=0; j<8; j++){  
-            HEART_UL &= ~(0x1U << (7-j)); 
+            STAR_UL &= ~(0x1U << (7-j)); 
             delay_ms(50);     
 
         }
             
         for (unsigned int j=0; j<8; j++){  
-           HEART_LL &= ~(0x1U << (j));  
+           STAR_LL &= ~(0x1U << (j));  
            delay_ms(50);    
         }
         
         for (unsigned int j=0; j<8; j++){  
-           HEART_LR &= ~(0x1U << (j)); 
+           STAR_LR &= ~(0x1U << (j)); 
             delay_ms(50);     
         }
 
         for (unsigned int j=0; j<8; j++){  
-            HEART_UR &= ~(0x1U << (j));
+            STAR_UR &= ~(0x1U << (j));
             delay_ms(50);      
 
         }
-        bh_all_off();
+        bs_all_off();
         delay_ms(50);
 
     } 
@@ -303,182 +305,182 @@ void bh_fullon_trace_ccw(unsigned int count)
 }
 
 /*********************************************************************************************/
-void bh_solid_flash(unsigned int count)
+void bs_solid_flash(unsigned int count)
 {
-    bh_all_off();
+    bs_all_off();
     for (unsigned int i=0; i<count; i++){
-        bh_all_on();
+        bs_all_on();
         delay_ms(250);
-        bh_all_off();      
+        bs_all_off();      
         delay_ms(250);
     }    
 
 }
 
 /*********************************************************************************************/
-void bh_trace_top_to_bottom(unsigned int count)
+void bs_trace_top_to_bottom(unsigned int count)
 {
-    bh_all_off();
+    bs_all_off();
     for (unsigned int i=0; i<count; i++){
             for (unsigned int j=0; j<8; j++){  
-            HEART_UR &= ~(0x1U << (7-j));
-            HEART_UL &= ~(0x1U << (7-j));
+            STAR_UR &= ~(0x1U << (7-j));
+            STAR_UL &= ~(0x1U << (7-j));
             delay_ms(50);      
 
         }
 
         for (unsigned int j=0; j<8; j++){  
-            HEART_LR &= ~(0x1U << (7-j));
-            HEART_LL &= ~(0x1U << j);  
+            STAR_LR &= ~(0x1U << (7-j));
+            STAR_LL &= ~(0x1U << j);  
             delay_ms(50);
         }
-        bh_all_off();
+        bs_all_off();
         delay_ms(50);
     }
 }
 
 /*********************************************************************************************/
-void bh_trace_bottom_to_top(unsigned int count)
+void bs_trace_bottom_to_top(unsigned int count)
 {
-    bh_all_off();
+    bs_all_off();
     for (unsigned int i=0; i<count; i++){
         for (unsigned int j=0; j<8; j++){  
-           HEART_LL &= ~(0x1U << (7-j));
-           HEART_LR &= ~(0x1U << j);  
+           STAR_LL &= ~(0x1U << (7-j));
+           STAR_LR &= ~(0x1U << j);  
            delay_ms(50);    
         }
 
 
         for (unsigned int j=0; j<8; j++){  
-            HEART_UL &= ~(0x1U << j);
-            HEART_UR &= ~(0x1U << j); 
+            STAR_UL &= ~(0x1U << j);
+            STAR_UR &= ~(0x1U << j); 
             delay_ms(50);     
 
         }
-        bh_all_off();
+        bs_all_off();
         delay_ms(50);
     }    
 
 }
 
 /*********************************************************************************************/
-void bh_trace_top_and_bottom(unsigned int count)
+void bs_trace_top_and_bottom(unsigned int count)
 {
-    bh_all_off();
+    bs_all_off();
     for (unsigned int i=0; i<count; i++){
             for (unsigned int j=0; j<8; j++){  
-            HEART_UR &= ~(0x1U << (7-j));
-            HEART_UL &= ~(0x1U << (7-j));
-            HEART_LL &= ~(0x1U << (7-j));
-            HEART_LR &= ~(0x1U << j);
+            STAR_UR &= ~(0x1U << (7-j));
+            STAR_UL &= ~(0x1U << (7-j));
+            STAR_LL &= ~(0x1U << (7-j));
+            STAR_LR &= ~(0x1U << j);
             delay_ms(100);      
 
         }
 
-        bh_all_off();
+        bs_all_off();
         delay_ms(50);
     }
 }
 
 /*********************************************************************************************/
-void bh_trace_middle_to_bt(unsigned int count)
+void bs_trace_middle_to_bt(unsigned int count)
 {
-    bh_all_off();
+    bs_all_off();
     for (unsigned int i=0; i<count; i++){
             for (unsigned int j=0; j<8; j++){  
-            HEART_UR &= ~(0x1U << j);
-            HEART_UL &= ~(0x1U << j);
-            HEART_LL &= ~(0x1U << j);
-            HEART_LR &= ~(0x1U << (7-j));
+            STAR_UR &= ~(0x1U << j);
+            STAR_UL &= ~(0x1U << j);
+            STAR_LL &= ~(0x1U << j);
+            STAR_LR &= ~(0x1U << (7-j));
             delay_ms(100);      
 
         }
 
-        bh_all_off();
+        bs_all_off();
         delay_ms(50);
     }
 }
 
 /*********************************************************************************************/
-void bh_trace_left_to_right(unsigned int count)
+void bs_trace_left_to_right(unsigned int count)
 {
-    bh_all_off();
+    bs_all_off();
     for (unsigned int i=0; i<count; i++){
 
             for (unsigned int j=0; j<8; j++){  
-            HEART_LL &= ~(0x1U << j); 
-            HEART_UL &= ~(0x1U << j);
+            STAR_LL &= ~(0x1U << j); 
+            STAR_UL &= ~(0x1U << j);
             delay_ms(50);      
         }
 
         for (unsigned int j=0; j<8; j++){  
-            HEART_LR &= ~(0x1U << j);
-            HEART_UR &= ~(0x1U << (7-j)); 
+            STAR_LR &= ~(0x1U << j);
+            STAR_UR &= ~(0x1U << (7-j)); 
             delay_ms(50);
         }
-        bh_all_off();
+        bs_all_off();
         delay_ms(50);
     }
 }
 /*********************************************************************************************/
 
-void bh_trace_right_to_left(unsigned int count)
+void bs_trace_right_to_left(unsigned int count)
 {
-    bh_all_off();
+    bs_all_off();
     for (unsigned int i=0; i<count; i++){
         for (unsigned int j=0; j<8; j++){  
-           HEART_UR &= ~(0x1U << j); 
-           HEART_LR &= ~(0x1U << (7-j));  
+           STAR_UR &= ~(0x1U << j); 
+           STAR_LR &= ~(0x1U << (7-j));  
            delay_ms(50);    
         }
 
 
         for (unsigned int j=0; j<8; j++){  
-            HEART_UL &= ~(0x1U << (7-j));
-            HEART_LL &= ~(0x1U << (7-j));
+            STAR_UL &= ~(0x1U << (7-j));
+            STAR_LL &= ~(0x1U << (7-j));
             delay_ms(50);     
 
         }
-        bh_all_off();
+        bs_all_off();
         delay_ms(50);
     }    
 
 }
 
 /*********************************************************************************************/
-void bh_trace_left_and_right(unsigned int count)
+void bs_trace_left_and_right(unsigned int count)
 {
-    bh_all_off();
+    bs_all_off();
     for (unsigned int i=0; i<count; i++){
             for (unsigned int j=0; j<8; j++){  
-            HEART_UR &= ~(0x1U << (7-j));
-            HEART_UL &= ~(0x1U << (7-j));
-            HEART_LL &= ~(0x1U << (7-j));
-            HEART_LR &= ~(0x1U << j);
+            STAR_UR &= ~(0x1U << (7-j));
+            STAR_UL &= ~(0x1U << (7-j));
+            STAR_LL &= ~(0x1U << (7-j));
+            STAR_LR &= ~(0x1U << j);
             delay_ms(100);      
 
         }
 
-        bh_all_off();
+        bs_all_off();
         delay_ms(50);
     }
 }
 
 /*********************************************************************************************/
-void bh_trace_middle_to_lr(unsigned int count)
+void bs_trace_middle_to_lr(unsigned int count)
 {
-    bh_all_off();
+    bs_all_off();
     for (unsigned int i=0; i<count; i++){
             for (unsigned int j=0; j<8; j++){  
-            HEART_UR &= ~(0x1U << j);
-            HEART_UL &= ~(0x1U << j);
-            HEART_LL &= ~(0x1U << j);
-            HEART_LR &= ~(0x1U << (7-j));
+            STAR_UR &= ~(0x1U << j);
+            STAR_UL &= ~(0x1U << j);
+            STAR_LL &= ~(0x1U << j);
+            STAR_LR &= ~(0x1U << (7-j));
             delay_ms(100);      
 
         }
 
-        bh_all_off();
+        bs_all_off();
         delay_ms(50);
     }
 }
@@ -488,32 +490,32 @@ int main()
 {
 
 
-    bh_setup();
+    bs_setup();
 
     while(1)
     {                
   
 
-        bh_solid_flash(3);
+        bs_solid_flash(3);
         
-        bh_quadrant_chase(3);
-        bh_quadrant_dance(3);
+        bs_quadrant_chase(3);
+        bs_quadrant_dance(3);
         
-        bh_fullon_chase_cw(3);
-        bh_fullon_chase_ccw(3);
+        bs_fullon_chase_cw(3);
+        bs_fullon_chase_ccw(3);
         
-        bh_fullon_trace_cw(3);
-        bh_fullon_trace_ccw(3);
+        bs_fullon_trace_cw(3);
+        bs_fullon_trace_ccw(3);
         
-        bh_trace_top_to_bottom(3);
-        bh_trace_bottom_to_top(3);
-        bh_trace_top_and_bottom(3);
-        bh_trace_middle_to_bt(3);
+        bs_trace_top_to_bottom(3);
+        bs_trace_bottom_to_top(3);
+        bs_trace_top_and_bottom(3);
+        bs_trace_middle_to_bt(3);
 
-        bh_trace_left_to_right(3);
-        bh_trace_right_to_left(3);
-        bh_trace_left_and_right(3);
-        bh_trace_middle_to_lr(3);
+        bs_trace_left_to_right(3);
+        bs_trace_right_to_left(3);
+        bs_trace_left_and_right(3);
+        bs_trace_middle_to_lr(3);
 
         WDT_CONTR |= 1 << 4; // clear wdt
     }
